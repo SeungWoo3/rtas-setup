8778c5d388aa7294d0ad621082dea4464c7cbd09
SeungWoo3 <rohseungwoo@gmail.com>
2024-11-27 18:25:30 +0900

    minor fix

--- git_setup.sh
+++ git_setup.sh
@@ -3,4 +3,5 @@ git config --global user.name SeungWoo3
 git config --global credential.helper store
 
 # token
-# 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
+# ghp_WAI4vWV79Y8lLI
+# FKDnUEVkhlcFt80N2PNa5s
